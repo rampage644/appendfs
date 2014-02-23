633f80d3353aa1f39e61231695cfa13074497ca8
Sergei Turukin <rampage644@gmail.com>
2014-02-23 15:33:24 +0400

    Unmount fixed

--- test_lmdb.py
+++ test_lmdb.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
 # -*- coding:utf8 -*-
 
+import os
 import unittest
 import subprocess
 
+# relative mount point
 MOUNT_POINT = 'fs'
 
 
 class LMDBFilesystemTest(unittest.TestCase):
     def testDirectoryListing(self):
-        self.fail()
+        self.assertIn('.', os.listdir(MOUNT_POINT))
+        self.assertIn('..', os.listdir(MOUNT_POINT))
 
 
 if __name__ == '__main__':
     subprocess.check_output(['python', 'lmdb.py', MOUNT_POINT])
-    unittest.main()
-    subprocess.check_call(['fusermount', '-u', MOUNT_POINT])
+    try:
+        unittest.main()
+    finally:
+        subprocess.check_call(['fusermount', '-u', MOUNT_POINT])
